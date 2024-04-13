4fbd12e37991a08d2092eea61da12f8c291f2446
jeonjuwon <juwonjeon852@gmail.com>
2024-04-13 12:10:57 +0900

    [DOCS]주석 추가

--- app/src/main/java/com/example/recyclerviewexample/data/DataSource.kt
+++ app/src/main/java/com/example/recyclerviewexample/data/DataSource.kt
@@ -6,9 +6,10 @@ class DataSource {
     companion object{
         private var INSTANCE : DataSource? = null
 
-        // 싱글톤 객체를 한번 호출하고 없으면 DataSource반환, 있으면 생성된 인스턴스 반환
         fun getDataSoures() : DataSource{
+            // DatoSource::class 객체에 lock을 걸어 한번에 한 스레드에서만 실행 되도록 함
             return synchronized(DataSource::class) {
+                // 싱글톤 객체를 한번 호출하고 없으면 DataSource반환, 있으면 생성된 인스턴스 반환
                 val newInstance = INSTANCE ?: DataSource()
                 INSTANCE = newInstance
                 newInstance
